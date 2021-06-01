1e329fd6d48fecb090e9ccebc81ec1ae07a35080
naonao-program <gf19a012@oecu.jp>
2021-06-01 17:24:19 +0000

    投稿機能の実装完了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,20 @@
 class BooksController < ApplicationController
   def index
+    @book = Book.new
   end
 
   def create
-
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "successfully"
+      redirect_to book_path(@book)
+    else
+      render :index
+    end
   end
-
+  
+  private
+  def book_params
+    params.require(:book).permit(:title,:body)
+  end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,7 @@
 class Book < ApplicationRecord
+  with_options presence: true do
+    validates :title
+    validates :body
+  end
+
 end
