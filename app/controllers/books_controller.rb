80e0b7b9243f41faf6d64668507a294d046938c6
naonao-program <gf19a012@oecu.jp>
2021-06-02 03:49:05 +0000

    モデルの単体テストコードを実行エラーは出なかった

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -22,10 +22,9 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    if book.save
-
-      redirect_to book_path(params[:id]), notice: "Book was successfully updated."
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book), notice: "Book was successfully updated."
     else
       render :edit
     end
