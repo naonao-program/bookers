8025a4848bd549b8beb6f263d425bdfca0db2c01
naonao-program <gf19a012@oecu.jp>
2021-06-02 02:47:35 +0000

    [Finish] bookers-app1

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,25 +12,31 @@ class BooksController < ApplicationController
       render :index
     end
   end
-  
+
   def show
     @book = Book.find(params[:id])
   end
-  
+
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
     book = Book.find(params[:id])
     if book.save
-      
+
       redirect_to book_path(params[:id]), notice: "Book was successfully updated."
     else
       render :edit
     end
   end
-  
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
   private
   def book_params
     params.require(:book).permit(:title,:body)
