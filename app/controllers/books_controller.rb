58197d2e05f102bebaeb1a554d81f6251b8bad19
naonao-program <gf19a012@oecu.jp>
2021-06-02 02:09:38 +0000

    showページの作成完了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,6 +13,10 @@ class BooksController < ApplicationController
     end
   end
   
+  def show
+    @book = Book.find(params[:id])
+  end
+  
   private
   def book_params
     params.require(:book).permit(:title,:body)
